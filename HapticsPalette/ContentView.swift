19478693773fe9e0d4ad4e04e84366ab6bd90058
Issac Penn <pbbsignup@outlook.com>
2020-03-11 23:23:15 +0800

    Initial Commit

---
+++ HapticsPalette/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HapticsPalette
+//
+//  Created by Issac Penn on 3/11/20.
+//  Copyright © 2020 Issac Penn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
